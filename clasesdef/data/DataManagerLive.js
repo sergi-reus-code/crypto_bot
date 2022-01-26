f6eb96113570e50f910bc723ec41013e47e1ec21
sergi-reus-code <sergi@reusconstruccions.com>
2022-01-26 08:23:26 +0100

    as

--- clasesdef/data/DataManagerLive.js
+++ clasesdef/data/DataManagerLive.js
@@ -9,84 +9,65 @@ const client = Binance({
 
 
 module.exports = class DataManagerLive extends DataManager {
-  
-    // Constructor
-    constructor(symbol, shortPeriod, longPeriod, starts, ends) {
-      super(symbol, shortPeriod, longPeriod, starts, ends)
+  // Constructor
+  constructor(symbol, shortPeriod, longPeriod, starts, ends) {
+    super(symbol, shortPeriod, longPeriod, starts, ends);
+  }
 
-      
+  async init() {
+    console.log("sincronizando symbolo.....");
 
-    }
-    
-    async init() {
+    var candlesOptions = {
+      symbol: `${this.symbol}`,
+      limit: 200,
+      interval: `1m`,
+    };
 
-      console.log("sincronizando symbolo.....");
+    var data = await client.candles(candlesOptions);
 
-      var candlesOptions = {"symbol" : `${this.symbol}`, 
-      "limit" : 200, 
-      "interval" : `1m`
-      
-    }
-
-    
-  
-    var data = await client.candles(candlesOptions)
-
-
-
-    data.forEach(cc => {
-  
-      this.startTime.push(Number(cc.openTime))
-      this.open.push(Number(cc.open))
-      this.high.push(Number(cc.high))
-      this.low.push(Number(cc.low))
-      this.close.push(Number(cc.close))
-      this.volume.push(Number(cc.volume))
-      this.quoteVolume.push(Number(cc.quoteVolume))
-   
+    data.forEach((cc) => {
+      this.startTime.push(Number(cc.openTime));
+      this.open.push(Number(cc.open));
+      this.high.push(Number(cc.high));
+      this.low.push(Number(cc.low));
+      this.close.push(Number(cc.close));
+      this.volume.push(Number(cc.volume));
+      this.quoteVolume.push(Number(cc.quoteVolume));
     });
 
-    this.dataReady = true
+    this.dataReady = true;
+  }
 
-    }
+  update(cc) {
+    this.startTime.shift();
+    this.open.shift();
+    this.high.shift();
+    this.low.shift();
+    this.close.shift();
+    this.volume.shift();
+    this.quoteVolume.shift();
 
+    this.startTime.push(Number(cc.openTime));
+    this.open.push(Number(cc.open));
+    this.high.push(Number(cc.high));
+    this.low.push(Number(cc.low));
+    this.close.push(Number(cc.close));
+    this.volume.push(Number(cc.volume));
+    this.quoteVolume.push(Number(cc.quoteVolume));
+  }
 
-    update(cc){
+  // Getter
+  get getLastCandles() {
+    //return [this.calcArea(),0];
+  }
 
-      this.startTime.shift() 
-      this.open.shift() 
-      this.high.shift() 
-      this.low.shift() 
-      this.close.shift() 
-      this.volume.shift() 
-      this.quoteVolume.shift() 
+  // Method
+  getLastCandle() {
+    return [this.timeShort[this.timeShort.length - 1]];
+  }
 
-      this.startTime.push(Number(cc.openTime))
-      this.open.push(Number(cc.open))
-      this.high.push(Number(cc.high))
-      this.low.push(Number(cc.low))
-      this.close.push(Number(cc.close))
-      this.volume.push(Number(cc.volume))
-      this.quoteVolume.push(Number(cc.quoteVolume))
-
-    }
-  
-
-    // Getter
-    get getLastCandles() {
-      //return [this.calcArea(),0];
-    }
-    
-  
-    
-    // Method
-    getLastCandle() {
-      return [this.timeShort[this.timeShort.length - 1]];
-    }
-  
-    // Static method
-    static calcArea(width, height) {
-      return "pepe";
-    }
-  
-}
+  // Static method
+  static calcArea(width, height) {
+    return "pepe";
+  }
+};
